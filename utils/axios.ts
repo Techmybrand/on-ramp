0a870bc53eafcfa7dca86fc9b3474454509c47c4
new account <philipakhilome@gmail.com>
2023-03-31 22:56:58 +0100

    get API url from env

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
+const API_URL = process.env.API_URL || 'http://localhost:8080/api';
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: API_URL
 });
 
 export const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: API_URL,
   headers: { "content-Type": "application/json" }
   // withCredentials: true
 });
