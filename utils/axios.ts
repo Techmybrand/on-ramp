6dc173865c54e3d4147cdd1521e0280a27273bad
new account <philipakhilome@gmail.com>
2023-03-31 23:08:59 +0100

    get API url from env

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = process.env.API_URL || 'http://localhost:8080/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api';
 
 export default axios.create({
   baseURL: API_URL
